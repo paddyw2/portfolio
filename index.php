d1e1f0207437394bbce972edf56daf294cab05e8
Patrick <p.withams@gmail.com>
2015-06-23 14:21:45 -0600

    adding base files

---
+++ index.php
@@ -0,0 +1,46 @@
+<html>
+<head>
+    <meta charset="utf-8">
+    <title>Patrick S. Withams</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="style.css" />
+  <link rel="stylesheet" href="//maxcdn.bootstrapcdn.com/font-awesome/4.3.0/css/font-awesome.min.css">
+</head>
+
+<body>
+<div class="main-body">
+<div class="container">
+ 
+<h1>Patrick S. Withams</h1>
+   
+    <p>I am a writer, designer and web developer.</p>
+    <div class="project-container">
+        <a href="http://fontinspiration.com"><div id="hover" class="project1"></div></a>
+        <br>
+        <a href="http://code.patrickswithams.com"><div id="hover" class="project2"></div></a>
+    </div>
+     <p>If you'd like to find out more about what I do, get in contact.</p>
+    
+    <a href="mailto:pwithams@gmail.com"><div class="button"><p>Contact</p></div></a>
+    
+</div>    
+</div>
+    
+   
+    <div class="footer">
+        <div class="icons">
+            <a href="https://github.com/paddyw2"><i class="fa fa-github"></i></a>
+            <a href="https://dribbble.com/paddyw2"><i class="fa fa-dribbble"></i></a>
+        <a href="http://patrickwithams.tumblr.com/"><i class="fa fa-tumblr"></i></a>
+         <a href="https://ca.linkedin.com/pub/patrick-withams/84/b32/32a"><i class="fa fa-linkedin"></i></a>
+             <a href="http://codepen.io/paddyw2"><i class="fa fa-codepen"></i></a>
+        </div>
+    </div>
+    
+    
+    
+    
+    
+</body>
+
+</html>
