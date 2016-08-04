e93ad2da5ebdfc81656f17a18e81d5600b9f1683
paddyw2 <p.withams@gmail.com>
2016-08-03 20:29:18 -0600

    Fix post.php

--- post.php
+++ post.php
@@ -6,7 +6,8 @@ $message = $_POST['message'];
 $email = filter_var($email, FILTER_SANITIZE_EMAIL); // Sanitizing E-mail.
 // After sanitization Validation is performed
 if (filter_var($email, FILTER_VALIDATE_EMAIL)) {
-    $sendmessage = "Name: " + $name + ", Email: " + $email + ", Message: " + $message;
+
+    $sendmessage = "Name: " . $name . ", Email: " . $email . ", Message: " . $message;
     // Message lines should not exceed 70 characters (PHP rule), so wrap it.
     $sendmessage = wordwrap($sendmessage, 70);
     // Send mail by PHP Mail Function.
