441f9c157f5fb03aac733299f540ebe5688d2edc
paddyw2 <p.withams@gmail.com>
2016-08-03 20:55:42 -0600

    Add documentation to js and php

--- post.php
+++ post.php
@@ -1,22 +1,36 @@
 <?php
-$receiver = "admin@patrickwithams.com";
-$name = $_POST['name'];
-$email = $_POST['email'];
-$message = $_POST['message'];
-$email = filter_var($email, FILTER_SANITIZE_EMAIL); // Sanitizing E-mail.
-// After sanitization Validation is performed
-if (filter_var($email, FILTER_VALIDATE_EMAIL)) {
-    $sendmessage = "<strong>Name:</strong> " . $name . "<br><strong>Email:</strong> " . $email . "<br><strong>Message:</strong> " . $message;
+    // gather post parameters into variables
+    $receiver = "admin@patrickwithams.com";
+    $name = $_POST['name'];
+    $email = $_POST['email'];
+    $message = $_POST['message'];
 
-    $headers = "MIME-Version: 1.0\r\n";
-    $headers .= "Content-type: text/html; charset=iso-8859-1\r\n";
-    // Message lines should not exceed 70 characters (PHP rule), so wrap it.
-    $sendmessage = wordwrap($sendmessage, 70);
-    // Send mail by PHP Mail Function.
-    mail($receiver, "New Website Submission", $sendmessage, $headers);
-    echo "<p>Message sent</p>";
-} else {
-    echo "<p class=\"error\">Invalid email</p>";
-}
+    // sanitize email
+    $email = filter_var($email, FILTER_SANITIZE_EMAIL);
 
+    // check email format is valid 
+    if (filter_var($email, FILTER_VALIDATE_EMAIL)) {
+        // if data is valid, create html messge using
+        // post parameters
+        $sendmessage = "<strong>Name:</strong> "
+            . $name . "<br><strong>Email:</strong> "
+            . $email . "<br><strong>Message:</strong> "
+            . $message;
+        
+        // create necessary content-type headers
+        $headers = "MIME-Version: 1.0\r\n";
+        $headers .= "Content-type: text/html; charset=iso-8859-1\r\n";
+
+        // wrap content to satisfy php rule (if over 70 in length) 
+        $sendmessage = wordwrap($sendmessage, 70);
+
+        // send html message to receiver email with relevant headers
+        mail($receiver, "New Website Submission", $sendmessage, $headers);
+
+        // print success message
+        echo "<p>Message sent</p>";
+    } else {
+        // print failure message
+        echo "<p class=\"error\">Invalid email</p>";
+    }
 ?>
