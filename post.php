5ec2fcbe03c6079df882c5ceed1494f88b4232f1
paddyw2 <p.withams@gmail.com>
2016-08-03 19:55:39 -0600

    Fix success condition

--- post.php
+++ post.php
@@ -1,29 +1,16 @@
 <?php
 $receiver = "admin@patrickwithams.com";
-$incoming = "incoming@patrickwithams.com";
 $name = $_POST['name'];
 $email = $_POST['email'];
 $message = $_POST['message'];
 $email = filter_var($email, FILTER_SANITIZE_EMAIL); // Sanitizing E-mail.
 // After sanitization Validation is performed
 if (filter_var($email, FILTER_VALIDATE_EMAIL)) {
-    $subject = $incoming;
-    // To send HTML mail, the Content-type header must be set.
-    $headers = 'MIME-Version: 1.0' . "\r\n";
-    $headers .= 'Content-type: text/html; charset=iso-8859-1' . "\r\n";
-    $headers .= 'From:' . $incoming. "\r\n"; // Sender's Email
-    // $headers .= 'Cc:' . $email. "\r\n"; // Carbon copy to Sender
-    $template = '<div>'
-    . '<br/>New website submission<br/><br/>'
-    . 'Name:' . $name . '<br/>'
-    . 'Email:' . $email . '<br/>'
-    . 'Message:' . $message . '<br/><br/>'
-    . '</div>';
-    $sendmessage = $template;
+    $sendmessage = "Name: " + $name + ", Email: " + $email + ", Message: " + $message;
     // Message lines should not exceed 70 characters (PHP rule), so wrap it.
     $sendmessage = wordwrap($sendmessage, 70);
     // Send mail by PHP Mail Function.
-    mail($receiver, $subject, $sendmessage, $headers);
+    mail($receiver, "New Website Submission", $sendmessage);
     echo "<p>Message sent</p>";
 } else {
     echo "<p class=\"error\">Invalid email</p>";
